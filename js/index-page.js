bbf135fe01e7f7777291845f8e6dbd8da9deb9ee
Zhaobole <oymwzbl@163.com>
2025-09-10 20:38:30 +0800

    Update index-page.js via web editor

--- js/index-page.js
+++ js/index-page.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 try {
                     window.history.replaceState(
                         { search: keyword }, 
-                        `搜索: ${keyword} - LibreTV`, 
+                        `搜索: ${keyword} - 闪电影视`, 
                         window.location.href
                     );
                 } catch (e) {
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
             try {
                 window.history.replaceState(
                     { search: searchQuery }, 
-                    `搜索: ${searchQuery} - LibreTV`, 
+                    `搜索: ${searchQuery} - 闪电影视`, 
                     `/s=${encodeURIComponent(searchQuery)}`
                 );
             } catch (e) {
