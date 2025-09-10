cd1fa90f3805aab25bfc8184144506d7bb49f90a
Zhaobole <oymwzbl@163.com>
2025-09-10 20:37:17 +0800

    Update version-check.js via web editor

--- js/version-check.js
+++ js/version-check.js
@@ -37,8 +37,8 @@ async function checkForUpdates() {
         // 获取最新版本
         let latestVersion;
         const VERSION_URL = {
-            PROXY: 'https://ghfast.top/raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt',
-            DIRECT: 'https://raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt'
+            PROXY: 'https://ghfast.top/raw.githubusercontent.com/LibreSpark/闪电影视/main/VERSION.txt',
+            DIRECT: 'https://raw.githubusercontent.com/LibreSpark/闪电影视/main/VERSION.txt'
         };
         const FETCH_TIMEOUT = 1500;
         
@@ -148,7 +148,7 @@ function addVersionInfoToFooter() {
                 const updateBtn = versionElement.querySelector('span');
                 if (updateBtn) {
                     updateBtn.addEventListener('click', () => {
-                        window.open('https://github.com/LibreSpark/LibreTV', '_blank');
+                        window.open('https://github.com/LibreSpark/闪电影视', '_blank');
                     });
                 }
             }, 100);
